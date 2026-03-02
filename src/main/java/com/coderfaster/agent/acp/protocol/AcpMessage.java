f4f13fcf16df81b867d4c799e9f478622b7cddfc
lee <494037524@qq.com>
2026-03-03 01:16:13 +0800

    fix: 添加 @AllArgsConstructor 解决编译错误
    - @Builder 与 @Builder.Default 需要 @AllArgsConstructor 配合
    - 保留 @NoArgsConstructor 供 Jackson 反序列化使用

--- src/main/java/com/coderfaster/agent/acp/protocol/AcpMessage.java
+++ src/main/java/com/coderfaster/agent/acp/protocol/AcpMessage.java
@@ -1,6 +1,7 @@
 package com.coderfaster.agent.acp.protocol;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class AcpMessage {
     @Builder.Default
     private String jsonrpc = "2.0";
